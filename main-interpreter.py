e0fbcb621e4d76dcb55bf0aa33124a4a088e1d5b
Osamu-KJ <Kevin.jerebica@gmail.com>
2022-08-19 13:45:15 +0200

    Added the variables feature

--- main-interpreter.py
+++ main-interpreter.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 # global variables
-DEFAULT_FILE_LOCATION = "./programs/first.deadbeef"
+DEFAULT_FILE_LOCATION = "./programs/variables.deadbeef"
 
 # global functions
 def err(string): 
@@ -13,17 +13,54 @@ def err(string):
 def info(string):
     print (f"[0xdeadbeef info]: {string}")
 
+def func_output(location, name, message):
+    location = "0x{:08x}".format(location).upper() 
+    print (f"[{location} {name}] {message}")
+
+# interpreter global variables
+INTERPRETER_VARIABLES = {}
+
 # interpreter global functions 
-def printdb(string, interpreter): 
-    print (f"[0xdeadbeef output]: {string}")
+def printdb(parameters, interpreter): 
+    string = parameters[0]
+
+    func_output(interpreter.cursor, "printdb", f"{string}")
     interpreter.increase_cursor()
+
+def printvar(parameters, interpreter):
+    name = parameters[0]
+
+    if (name not in INTERPRETER_VARIABLES):
+        err(f"No variable named {name}")
+        return 
+
+    variable_val = INTERPRETER_VARIABLES[name]
+    func_output(interpreter.cursor, "printvar", f"'{name}'={variable_val}")
+    interpreter.increase_cursor()
+
+def setvar(parameters, interpreter):
+    name = parameters[0]
+    value = parameters[1]
+
+    if (name in INTERPRETER_VARIABLES):
+        err(f"Variable with name '{name}' already exists")
+        return 
+
+    INTERPRETER_VARIABLES[name] = value
+    interpreter.increase_cursor()
+
 
 # interpreter global variables
 SYSCALLS = {
         0: { 
             "func": printdb, 
-            "params": 2 
         },
+        1: {
+            "func": setvar,
+        },
+        2: {
+            "func": printvar, 
+        }
 }
 
 # interpreter classes
@@ -94,7 +131,7 @@ class DBInterpreter:
         cursor_command = self.commands_map.fetch_command(self.cursor)
         cursor_syscall = cursor_command.syscall
         parameters = cursor_command.parameters
-        self.syscalls[cursor_syscall]["func"](parameters[0], self)
+        self.syscalls[cursor_syscall]["func"](parameters, self)
 
 # main function
 def main():
